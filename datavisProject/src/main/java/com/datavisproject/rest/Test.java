1fa4f9a468bb492a0203ab9b551b58ff76542835
hilmi can yavuz <hilmi.yavuz@hva.nl>
2015-11-04 16:44:00 +0100

    mysql db test

--- datavisProject/src/main/java/com/datavisproject/rest/Test.java
+++ datavisProject/src/main/java/com/datavisproject/rest/Test.java
@@ -1,14 +1,21 @@
 package com.datavisproject.rest;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
 @Path("test")
 public class Test {
-
+	
     @GET
     public String test() throws JSONException {
         JSONObject json = new JSONObject();
@@ -18,5 +25,50 @@ public class Test {
         
         json.put("my projects",  arr);
         return json.toString();
+    }
+    
+    @Path("db")
+    @GET
+    public String dbTest() throws SQLException, JSONException{
+    	System.out.println("-------- MySQL JDBC Connection Testing ------------");
+
+    	try {
+    		Class.forName("com.mysql.jdbc.Driver");
+    	} catch (ClassNotFoundException e) {
+    		e.printStackTrace();
+    		return("Where is your MySQL JDBC Driver?");
+    	}
+
+    	System.out.println("MySQL JDBC Driver Registered!");
+    	Connection connection = null;
+
+    	try {
+    		connection = DriverManager
+    		.getConnection("jdbc:mysql://185.41.126.25:9150/Yavuz2datavis_db","Yavuz2datavis", "qwe123456");
+
+    	} catch (SQLException e) {
+    		e.printStackTrace();
+    		return "Connection Failed! Check output console";
+    	}
+
+    	if (connection != null) {
+    		//return "You made it, take control your database now!";
+    	} else {
+    		return "Failed to make connection!";
+    	}
+    	
+    	JSONArray arr = new JSONArray();
+    	
+    	Statement stmt = connection.createStatement() ;
+    	String query = "SELECT page_title FROM yavuz2datavis_db.page where page_title like 'Chicago%sky%';" ;
+    	ResultSet rs = stmt.executeQuery(query) ;
+    	while(rs.next()){
+    		String _result = rs.getString(1);
+    		arr.put(_result);
+    	}
+    	
+    	JSONObject ob = new JSONObject();
+    	ob.put("Chicago", arr);
+    	return ob.toString();
     }
 }
